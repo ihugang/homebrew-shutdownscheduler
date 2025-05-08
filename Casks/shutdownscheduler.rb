a34f9b4591b559144f7914455807a9cc1ed976c2
hugang <hugang@codans.com>
2025-05-08 14:29:28 +0800

    fix rb

--- Casks/shutdownscheduler.rb
+++ Casks/shutdownscheduler.rb
@@ -1,8 +1,8 @@
 cask "shutdownscheduler" do
-  version "1.0.0"
-  sha256 "<PUT_SHA256_HERE>"
+  version "1.00"
+  sha256 "4c04b57d02ee9aef855f0889226f06f4a6110ee6635a07871ec2da95d252b1fe"
 
-  url "https://github.com/ihugang/ShutdownScheduler/releases/download/v\#{version}/ShutdownScheduler.dmg"
+  url "https://github.com/ihugang/ShutdownScheduler/releases/download/v#{version}/ShutdownScheduler.dmg"
   name "ShutdownScheduler"
   desc "Lightweight macOS menu bar app to schedule shutdown or sleep"
   homepage "https://github.com/ihugang/ShutdownScheduler"
